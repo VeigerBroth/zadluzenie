0b9dd790dd938d42e7a724a7aff859fe1744a094
VeigerBroth <cybulex@wp.pl>
2021-07-27 21:25:30 +0200

    Add changes from topi-chan and create enter_value function.

--- calc_credit.py
+++ calc_credit.py
@@ -1,24 +1,35 @@
-inflation_value = [1.59282448436825, -0.453509101198007, 2.32467171712441, 1.26125440724877, 1.78252628571251, 2.32938454145522, 1.50222984223283, 1.78252628571251, 2.32884899407637, 0.616921348207244, 2.35229588637833, 0.337779545187098, 1.57703524727525, -0.292781442607648, 2.48619659017508, 0.267110317834564, 1.41795267229799, 1.05424326726375, 1.4805201044812, 1.57703524727525, -0.077420690314702, 1.16573339872354, -0.404186717638335, 1.49970852083123]
-month = ['styczniu', 'lutym', 'marcu', 'kwietniu', 'maju', 'czerwcu', 'lipcu', 'sierpniu', 'wrześniu', 'październiku', 'listopadzie', 'grudniu']*2
+inflation_value = [1.59282448436825, -0.453509101198007, 2.32467171712441,
+1.26125440724877, 1.78252628571251, 2.32938454145522, 1.50222984223283,
+1.78252628571251, 2.32884899407637, 0.616921348207244, 2.35229588637833,
+0.337779545187098, 1.57703524727525, -0.292781442607648, 2.48619659017508,
+0.267110317834564, 1.41795267229799, 1.05424326726375, 1.4805201044812,
+1.57703524727525, -0.077420690314702, 1.16573339872354, -0.404186717638335,
+1.49970852083123]
 
-credit = input('Podaj początkową wysokość kredytu: ')
-percent = input('Podaj wysokość oprocentowania: ')
-installment = input('Podaj wartość raty: ')
+month = ['styczniu', 'lutym', 'marcu', 'kwietniu', 'maju', 'czerwcu', 'lipcu',
+'sierpniu', 'wrześniu', 'październiku', 'listopadzie', 'grudniu']*2
+        
+def enter_value(input_message, error_message, input_type_convertion):
+    while True:
+        try:
+            if input_type_convertion is int:
+                enter_value = int(input(input_message))
+                break
+            elif input_type_convertion is float:
+                enter_value = float(input(input_message))
+                break
+        except:
+            print(error_message)
+            
+    return enter_value
+            
+credit = enter_value('Podaj początkową wartość kredytu: ', 'Niepoprawna wartość wysokości kredytu!', float)
+percent = enter_value('Podaj wysokość oprocentowania: ', 'Niepoprawna wartość wysokości oprocentowania!', float)
+installment = enter_value('Podaj wartość raty: ', 'Niepoprawna wartość wysokości raty!', float)
 
-try:
-	a = float(credit)
-	b = float(percent)
-	c = float(installment)
-
-except:
-	a, b, c = ('' ,'' , '')
-	
-
-if (isinstance(a, float) and isinstance(b, float) and isinstance(c, float)) == True and (a and b and c) != 0:
-	for i in range(24):
-		calc = (1+((inflation_value[i]+b)/1200))*a-c
-		calc_less = a-calc
-		a = calc
-		print('W '+month[i]+' twoja pozostała kwota kredytu to '+str(calc)+', to '+str(calc_less)+' mniej niz w poprzednim miesiącu\n')
-else:
-	print('Jedna z wartości jest ujemna, zerowa lub jest tesktem!')
+for i in range(24):
+   calc = (1+((inflation_value[i]+percent)/1200))*credit-installment
+   calc_less = credit-calc
+   credit = calc
+   print(f'W {month[i]} twoja pozostała kwota kredytu to {calc}' 
+   +f' to {calc_less} mniej niz w poprzednim miesiącu \n')
